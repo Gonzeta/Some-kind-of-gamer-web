c3502c980346cab66176eea87afbb7d0584069f7
Gonzeta <164637561+Gonzeta@users.noreply.github.com>
2025-04-09 21:16:17 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 // Lista de imágenes y descripciones
 const juegos = [
-    { imagen: "juego1.jpg", titulo: "R.E.P.O", descripcion: "es una experiencia de horror en línea cooperativa que te mantendrá al borde del asiento, donde tú y hasta cinco compañeros se zambullen en lugares escalofriantes para buscar artefactos extraños para un benefacto", precio: "$5.79" },
-    { imagen: "juego2.jpg", titulo: "Schedule 1", descripcion: "Desde un pequeño traficante de drogas hasta un capo, fabrica y distribuye una amplia gama de drogas por la desaliñada ciudad de Hyland Point. Expande tu imperio con propiedades, negocios, empleados y mucho más.", precio: "$10.49" },
-    { imagen: "juego3.jpg", titulo: "Kingdome Come: Deliverance 2", descripcion: "Un emocionante RPG de acción con mucha narrativa y un exuberante mundo abierto situado en la Europa medieval del siglo XV. No te pierdas la aventura medieval definitiva donde vivirás un viaje de proporciones épicas de la mano del joven Henry", precio: "$44.99" }
+    { imagen: "IMG/juego1.jpg", titulo: "R.E.P.O", descripcion: "es una experiencia de horror en línea cooperativa que te mantendrá al borde del asiento, donde tú y hasta cinco compañeros se zambullen en lugares escalofriantes para buscar artefactos extraños para un benefacto", precio: "$5.79" },
+    { imagen: "IMG/juego2.jpg", titulo: "Schedule 1", descripcion: "Desde un pequeño traficante de drogas hasta un capo, fabrica y distribuye una amplia gama de drogas por la desaliñada ciudad de Hyland Point. Expande tu imperio con propiedades, negocios, empleados y mucho más.", precio: "$10.49" },
+    { imagen: "IMG/juego3.jpg", titulo: "Kingdome Come: Deliverance 2", descripcion: "Un emocionante RPG de acción con mucha narrativa y un exuberante mundo abierto situado en la Europa medieval del siglo XV. No te pierdas la aventura medieval definitiva donde vivirás un viaje de proporciones épicas de la mano del joven Henry", precio: "$44.99" }
 ];
 
 let indiceActual = 0;
